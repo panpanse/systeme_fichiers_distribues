a6417817d7fad92d2ff3714c7b26c577aeaaf252
Florent Lévigne <florent.levigne@mailoo.org>
2011-03-15 11:35:47 +0100

    maj script dep gluster, dep moosefs, bench (w petits fichier)

--- mooseFs/chunkServer.rb
+++ mooseFs/chunkServer.rb
@@ -1,11 +1,12 @@
-#!/bin/bin/ruby -w
+#!/usr/bin/ruby -w
 
 serverMasterIp = "#{ARGV[0]}"
-numberOfChunk = "#{ARGV[1]}"
-numOfChunk = "#{ARGV[2]}"
+numberOfChunk = "#{ARGV[1]}".to_i
+numOfChunk = "#{ARGV[2]}".to_s
 
 # placement au bon endroit
-`cd /usr/src/mfs-1.6.20-2`
+Dir.chdir("/usr/src/mfs-1.6.20-2")
+#`cd /usr/src/mfs-1.6.20-2`
 
 # compilation avec les options qui vont bien
 `./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var/lib --with-default-user=mfs --with-default-group=mfs --disable-mfsmaster`
@@ -15,21 +16,25 @@ numOfChunk = "#{ARGV[2]}"
 `make install`
 
 # "creation" des fichiers de conf
-`cd /etc`
+Dir.chdir("/etc")
 `cp mfschunkserver.cfg.dist mfschunkserver.cfg`
 `cp mfshdd.cfg.dist mfshdd.cfg`
 
 # conf fichier /etc/mfshdd.cfg
-`mkdir /tmp/mfschunks#{numOfChunk}`
-`chown -R mfs:mfs /tmp/mfschunks#{numOfChunk}`
+`mkdir /mnt/mfschunks#{numOfChunk}`
+#`mkdir /tmp/mfschunks#{numOfChunk}`
+
+#`chown -R mfs:mfs /tmp/mfschunks#{numOfChunk}`
+`chown -R mfs:mfs /mnt/mfschunks#{numOfChunk}`
 
 numberOfChunk.times do |i|
 	numChunk = i + 1
-	`echo "/tmp/mfschunks#{numChunk}" >> /etc/mfshdd.cfg`
+	#`echo "/tmp/mfschunks#{numChunk}" >> /etc/mfshdd.cfg`
+	`echo "/mnt/mfschunks#{numChunk}" >> /etc/mfshdd.cfg`
 end
 
 # ajout du serveur maitre dans /etc/hosts
-`echo "$serverMasterIp mfsmaster" >> /etc/hosts`
+`echo "#{serverMasterIp} mfsmaster" >> /etc/hosts`
 
 # demarrage du serveur
 `/usr/sbin/mfschunkserver start`
