cb5d0d10f635ae15def774ca9b472fa88db2634a
Panpanse <max.douheret@gmail.com>
2011-03-22 10:24:05 +0100

    modif script meta.sh

--- metascript/meta.sh
+++ metascript/meta.sh
@@ -12,47 +12,65 @@ fi
 
 nb_serveur=$1
 
+
+# gluster 5 serveurs
+cd ~/glusterFS
+echo "./deploiementGluster.rb $nb_serveur"
+./deploiementGluster.rb $nb_serveur
+
+# benchmark sur 1 client
+cd ~/benchmark
+./benchmark.rb 1 "~/resOfBench/gluster$nb_serveur" ~/glusterFS/listOfClients /media/glusterfs
+# benchmark sur 5 clients
+./benchmark.rb 5 "~/resOfBench/gluster$nb_serveur" ~/glusterFS/listOfClients /media/glusterfs
+# benchmark sur 20 clients 
+./benchmark.rb 20 "~/resOfBench/gluster$nb_serveur" ~/glusterFS/listOfClients /media/glusterfs
+# benchmark sur 50 clients 
+./benchmark.rb 50 "~/resOfBench/gluster$nb_serveur" ~/glusterFS/listOfClients /media/glusterfs
+
+
 # mooseFs 5 serveurs
 cd ~/mooseFs
 ./deploiementMoose.rb $nb_serveur
 
-## benchmark sur 2 clients 
+# benchmark sur 2 clients 
 cd ~/benchmark
-./benchmark.rb 2 "~/resOfBench/moose$nbserveur" ~/mooseFs/listOfClients /media/mfs
-# benchmark sur 3 clients 
-./benchmark.rb 3 "~/resOfBench/moose$nbserveur" ~/mooseFs/listOfClients /media/mfs
-## benchmark sur 5 clients 
+#echo "./benchmark.rb 2 \"~/resOfBench/moose$nb_serveur\" ~/mooseFs/listOfClients /media/mfs"
+./benchmark.rb 1 "~/resOfBench/moose$nb_serveur" ~/mooseFs/listOfClients /media/mfs
+# benchmark sur 5 clients 
+./benchmark.rb 5 "~/resOfBench/moose$nb_serveur" ~/mooseFs/listOfClients /media/mfs
+# benchmark sur 20 clients 
+./benchmark.rb 20 "~/resOfBench/moose$nb_serveur" ~/mooseFs/listOfClients /media/mfs
+# benchmark sur 50 clients 
+./benchmark.rb 50 "~/resOfBench/moose$nb_serveur" ~/mooseFs/listOfClients /media/mfs
+
+
+# NFS
+cd ~/NFS
+echo "./deploiementNFS.rb"
+./deploiementNFS.rb
+
+# benchmark sur 1 client
 cd ~/benchmark
-./benchmark.rb 5 "~/resOfBench/moose$nbserveur" ~/mooseFs/listOfClients /media/mfs
-# benchmark sur 10 clients 
-./benchmark.rb 10 "~/resOfBench/moose$nbserveur" ~/mooseFs/listOfClients /media/mfs
+./benchmark.rb 1 "~/resOfBench/gluster$nb_serveur" ~/NFS/listOfNodes /tmp/partage
+# benchmark sur 5 clients 
+./benchmark.rb 5 "~/resOfBench/gluster$nb_serveur" ~/NFS/listOfNodes /tmp/partage
+# benchmark sur 20 clients 
+./benchmark.rb 20 "~/resOfBench/gluster$nb_serveur" ~/NFS/listOfNodes /tmp/partage
+# benchmark sur 50 clients 
+./benchmark.rb 50 "~/resOfBench/gluster$nb_serveur" ~/NFS/listOfNodes /tmp/partage
+
 
 # ceph 5 serveurs
 cd ~/cephFS
-./deploiementGluster.rb $nbserveur
+./deploiementCeph.rb $nb_serveur
 
-# benchmark sur 2 clients 
+# benchmark sur 1 client
 cd ~/benchmark
-./benchmark.rb 2 "~/resOfBench/ceph$nbserveur" ~/cephFS/listOfClients /ceph
-# benchmark sur 3 clients 
-./benchmark.rb 3 "~/resOfBench/ceph$nbserveur" ~/cephFS/listOfClients /ceph
+./benchmark.rb 1 "~/resOfBench/ceph$nb_serveur" ~/cephFS/listOfClients /ceph
 # benchmark sur 5 clients 
-cd ~/benchmark
-./benchmark.rb 5 "~/resOfBench/ceph$nbserveur" ~/cephFS/listOfClients /ceph
-# benchmark sur 10 clients 
-./benchmark.rb 10 "~/resOfBench/ceph$nbserveur" ~/cephFS/listOfClients /ceph
-
-# gluster 5 serveurs
-cd ~/glusterFS
-./deploiementGluster.rb $nbserveur
-
-# benchmark sur 2 clients 
-cd ~/benchmark
-./benchmark.rb 2 "~/resOfBench/gluster$nbserveur" ~/glusterFS/listOfClients /media/glusterfs
-# benchmark sur 3 clients 
-./benchmark.rb 3 "~/resOfBench/gluster$nbserveur" ~/glusterFS/listOfClients /media/glusterfs
-# benchmark sur 5 clients 
-cd ~/benchmark
-./benchmark.rb 5 "~/resOfBench/gluster$nbserveur" ~/glusterFS/listOfClients /media/glusterfs
-# benchmark sur 10 clients 
-./benchmark.rb 10 "~/resOfBench/gluster$nbserveur" ~/glusterFS/listOfClients /media/glusterfs
+./benchmark.rb 5 "~/resOfBench/ceph$nb_serveur" ~/cephFS/listOfClients /ceph
+# benchmark sur 20 clients 
+./benchmark.rb 20 "~/resOfBench/ceph$nb_serveur" ~/cephFS/listOfClients /ceph
+# benchmark sur 50 clients 
+./benchmark.rb 50 "~/resOfBench/ceph$nb_serveur" ~/cephFS/listOfClients /ceph
